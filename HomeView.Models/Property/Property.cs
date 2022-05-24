649c7c7c0c1c6628bf2e198707348d43658f5858
Joshua Sonde (sondej) <joshua_sonde@icloud.com>
2022-05-24 03:10:51 +0100

    Property Search
    Added new models, repo and controller for property search

---
+++ HomeView.Models/Property/Property.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeView.Models.Property
+{
+    public class Property
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PropertyType { get; set; }
+        public int Price { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public string Postcode { get; set; }
+    }
+}
