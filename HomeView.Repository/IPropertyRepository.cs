97867f8eed53934c0840d9838e46c4187a42e27e
tay-g97 <taylorgeorgeuk@yahoo.co.uk>
2022-05-24 17:12:50 +0100

    re-added property files

---
+++ HomeView.Repository/IPropertyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HomeView.Models.Property;
+
+namespace HomeView.Repository
+{
+    public interface IPropertyRepository
+    {
+        public Task<List<PropertySearch>> SearchProperties(string location, string propertyType, string keywords, 
+            int minPrice, int maxPrice, int minBeds, int maxBeds,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ HomeView.Repository/PropertyRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using HomeView.Models.Property;
+using Microsoft.Extensions.Configuration;
+
+namespace HomeView.Repository
+{
+    public class PropertyRepository : IPropertyRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public PropertyRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<List<Property>> SearchProperties(PropertySearch propertySearch)
+        {
+
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Location", typeof(string));
+            dataTable.Columns.Add("PropertyType", typeof(string));
+            dataTable.Columns.Add("Keywords", typeof(string));
+            dataTable.Columns.Add("MinPrice", typeof(int));
+            dataTable.Columns.Add("MaxPrice", typeof(int));
+            dataTable.Columns.Add("MinBeds", typeof(int));
+            dataTable.Columns.Add("MaxBeds", typeof(int));
+
+            dataTable.Rows.Add(
+                propertySearch.Location,
+                propertySearch.PropertyType,
+                propertySearch.Keywords,
+                propertySearch.MinPrice,
+                propertySearch.MaxPrice,
+                propertySearch.MinBeds,
+                propertySearch.MaxBeds
+            );
+            IEnumerable<Property> properties;
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+              properties = await connection.QueryAsync<Property>("Property_Search",
+                    new { PropertySearch = dataTable.AsTableValuedParameter("dbo.PropertySearchType") },
+                    commandType: CommandType.StoredProcedure);
+            }
+
+            return properties.ToList();
+        }
+    }
+}
