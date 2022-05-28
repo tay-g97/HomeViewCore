b50c59ddcf97c102bd744c84d1d31e9f2498114d
Joshua Sonde (sondej) <joshua_sonde@icloud.com>
2022-05-28 18:56:00 +0100

    property search
    got property search functional, made some changes its functionality, made all parameters optional to user

--- HomeView.Models/Property/Property.cs
+++ HomeView.Models/Property/Property.cs
@@ -2,10 +2,10 @@
 {
     public class Property
     {
-        public string Address { get; set; }
+        public string PropertyAddress { get; set; }
         public string City { get; set; }
         public string PropertyType { get; set; }
-        public int Price { get; set; }
+        public int GuidePrice { get; set; }
         public int Bedrooms { get; set; }
         public int Bathrooms { get; set; }
         public string Postcode { get; set; }
--- HomeView.Models/Property/PropertySearch.cs
+++ HomeView.Models/Property/PropertySearch.cs
@@ -7,11 +7,8 @@ namespace HomeView.Models.Property
 {
     public class PropertySearch
     {
-        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; }
-        [Required(ErrorMessage = "PropertyType is required")]
         public string PropertyType { get; set; }
-        [Required(ErrorMessage = "Keywords are required")]
         public string Keywords { get; set; }
         public int MinPrice { get; set; }
         public int MaxPrice { get; set; }
